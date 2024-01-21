9d20e2cd12b2b808c56ff433ab673e6619ae0254
boltxyz <scriptxegemen@gmail.com>
2024-01-21 05:57:02 +0300

    test commit

--- src/main/java/net/mangoland/economy/Main.java
+++ src/main/java/net/mangoland/economy/Main.java
@@ -2,6 +2,6 @@ package net.mangoland.economy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello wosrld!");
     }
 }
